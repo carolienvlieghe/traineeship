4f641c4ab49ccc3b58fc0903614cab0b7a64816a
carolienvlieghe <50843469+carolienvlieghe@users.noreply.github.com>
2020-02-04 21:06:46 +0100

    change rownames AnnotatedDataFrame to fcs keywords

--- csv2fcs.R
+++ csv2fcs.R
@@ -23,9 +23,20 @@ output.folder <- "D:/school/Stage officieel/Output/"
 dir.create(path = output.folder)
 input.folder <- "D:/school/Stage officieel/Input/"
 
-# List the .analysis and csv files in the input folder
+# Zip and unzip the .analysis container to extract xml and fcs
+new.file <- gsub("analysis","zip", fcs.path)
+file.rename(from = fcs.path, to = new.file) # file is now a zipped folder
+zip.path <- list.files(path= input.folder, pattern = "\\.zip$", full.names=TRUE)
+zip.path.name <- basename(zip.path)
+zip.path.name.1 <- substr(zip.path.name,start=1,stop = nchar(zip.path.name)-4)
+file.name.zip <- substr(zip.path, start = 1, stop = nchar(zip.path)-4) 
+dir.create(path = file.name.zip) # make folder to extract files from zipped folder
+unzip(zip.path, exdir = file.name.zip) # unzip the folder
+
+# List the .analysis, csv and fcs files in the input folder
 fcs.path <- list.files(path= input.folder, pattern = "\\.analysis$", full.names=TRUE)
 csv.path <- list.files(path= input.folder, pattern = "\\.csv$", full.names=TRUE)
+file.name.fcs <- list.files(path = file.name.zip, pattern = "\\.fcs$", full.names = TRUE)
 
 # assign the output for the new fcs file 
 csv.base <- basename(csv.path)
@@ -33,13 +44,26 @@ base <- substr(csv.base, start = 1, stop = nchar(csv.base)-4) # keep only the na
 output.file <- paste(date.format, base, sep = " - ") # assign a new name to the new fcs file
 output.file.path <- paste0(output.folder, output.file, ".fcs") # assign the whole path to the new fcs file
 
+
 ### Read the files ###
 # Read the csv file
 csv.file <- read.csv(csv.path) # class = dataframe
 
-### Make flowFrame ###
-csvmatrix <- as.matrix(csv) # turn csv dataframe into a matrix 
+# Read fcs file dataset 1 in FCS 2.0 format (LMD from navios consists of 2 datasets, 1: FCS2.0 and 2: FCS3.0)
+fcs2 <- read.FCS(file.name.fcs,dataset=1)
+
+
+### Make flowFrame from csv ###
+csvmatrix <- as.matrix(csv.file) # turn csv dataframe into a matrix 
 ff <- new("flowFrame",exprs=csvmatrix) # make new flowFrame from csvmatrix
+
+# Change rownames of annotated dataframe (look at it with: pData(parameters(flowFrame))) to fcs keyword $Pn
+rows <- c()
+for (i in c(1:16)) {
+  rows <- append(rows, paste0("$P",i))
+}
+rownames(pData(parameters(ff))) <- rows
+
 
 ### Write to flowFrame to FCS###
 write.FCS(x = ff, output.file.path)
